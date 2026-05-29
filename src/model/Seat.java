9131352accea7720301dc0fb549a7514da53b210
apffxhsls <gyeji975@gmail.com>
2026-05-29 14:47:20 +0900

    feat: Seat class 생성

---
+++ src/model/Seat.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Seat {
+    private String seatId;      
+    private boolean isReserved; 
+
+    public Seat(String seatId) {
+        this.seatId = seatId;
+        this.isReserved = false;
+    }
+
+    public String getSeatId() { return seatId; }
+    public boolean isReserved() { return isReserved; }
+
+    public synchronized boolean reserve() {
+        if (this.isReserved) {
+            return false; 
+        }
+        this.isReserved = true;
+        return true;
+    }
+
+    public synchronized void cancel() {
+        this.isReserved = false;
+    }
+}
